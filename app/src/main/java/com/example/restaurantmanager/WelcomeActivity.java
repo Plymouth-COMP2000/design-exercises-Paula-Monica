61ad96385c57d19ad478d91a1cd9aa2f7d71641d
Paula Monica <paula.monica@students.plymouth.ac.uk>
2025-10-26 03:23:34 +0000

    Implemented homepage UI

---
+++ app/src/main/java/com/example/restaurantmanager/WelcomeActivity.java
@@ -0,0 +1,23 @@
+package com.example.restaurantmanager;
+
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    //UI Components
+    private Button loginButton;
+    private Button guestAccountButton;
+    private Button staffCreateAccountButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        loginButton = findViewById(R.id.loginButton);
+        guestAccountButton = findViewById(R.id.guestAccountButton);
+        staffCreateAccountButton = findViewById(R.id.staffCreateAccountButton);
+    }
+}
