361353bfcae070e6047ea9d5ed32c30eb8114ded
Paula Monica <paula.monica@students.plymouth.ac.uk>
2025-10-27 20:42:12 +0000

    Implemented UI for Login page

--- app/src/main/java/com/example/restaurantmanager/SuccessfulSignupActivity.java
+++ app/src/main/java/com/example/restaurantmanager/SuccessfulSignupActivity.java
@@ -2,7 +2,6 @@ package com.example.restaurantmanager;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SuccessfulSignupActivity extends AppCompatActivity {
