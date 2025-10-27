effada2e21ab2b0ee9349298ca2b4279ac85c320
Paula Monica <paula.monica@students.plymouth.ac.uk>
2025-10-27 19:14:25 +0000

    Implemented UI successful Signup confirmation page

---
+++ app/src/main/java/com/example/restaurantmanager/SuccessfulSignupActivity.java
@@ -0,0 +1,18 @@
+package com.example.restaurantmanager;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SuccessfulSignupActivity extends AppCompatActivity {
+    private Button returnButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_confirm);
+        returnButton = findViewById(R.id.confirmButton);
+
+    }
+}
