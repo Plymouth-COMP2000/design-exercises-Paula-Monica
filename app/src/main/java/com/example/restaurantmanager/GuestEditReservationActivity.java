0518c3365648e7bb679751a695236aba5fadbcdb
Paula Monica <paula.monica@students.plymouth.ac.uk>
2025-11-03 06:36:00 +0000

    Implemented edit reservation UI

--- app/src/main/java/com/example/restaurantmanager/GuestEditReservationActivity.java
+++ app/src/main/java/com/example/restaurantmanager/GuestEditReservationActivity.java
@@ -7,38 +7,33 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GuestNewReservationActivity extends AppCompatActivity {
+public class GuestEditReservationActivity extends AppCompatActivity {
 
     private ImageView backButton;
     private Spinner numberOfPeopleSpinner;
     private EditText datePicker;
     private EditText timePicker;
-    private Button confirmButton;
-
     private TextView peopleWarning;
-
     private TextView dateWarning;
-
     private TextView timeWarning;
+    private Button saveChangesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guest_make_reservation);
+        setContentView(R.layout.activity_edit_reservation);
 
         backButton = findViewById(R.id.backButton);
         numberOfPeopleSpinner = findViewById(R.id.numberOfPeopleSpinner);
         datePicker = findViewById(R.id.datePicker);
         timePicker = findViewById(R.id.timePicker);
-        confirmButton = findViewById(R.id.confirmButton);
         peopleWarning = findViewById(R.id.peopleWarning);
         dateWarning = findViewById(R.id.dateWarning);
         timeWarning = findViewById(R.id.timeWarning);
+        saveChangesButton = findViewById(R.id.saveChangesButton);
 
-        // Set up spinner with number of people options
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this,
                 R.array.number_of_people_array,
